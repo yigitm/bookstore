3493a80a97d4d0628fd46b31270b3255c072f879
Yigit Mersin <yigitmersin@gmail.com>
2022-01-18 03:48:45 +0300

    Fix linter

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Route, Routes, Link,
+} from 'react-router-dom';
 import Books from './book';
 import Category from './category';
 
