e495819967b5eb99fce6b9dcfc63517a27e4a4fa
Yigit Mersin <yigitmersin@gmail.com>
2022-01-19 21:19:25 +0300

    Add remove book

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { delBook } from '../redux/books/books';
 
 const Booklist = () => {
   const books = useSelector((state) => state.booksReducer);
+  const dispatch = useDispatch();
+
+  const removeBook = (e) => {
+    dispatch(delBook(e.target.parentElement.id));
+  };
+
   const displayB = books.map((book) => {
     return (
-      <div key={book.id} className="book">
+      <div key={book.id} id={book.id} className="book">
         <p>{book.title}</p>
-        <button type="button">Remove</button>
+        <button type="button" onClick={(e) => removeBook(e)}>
+          Remove
+        </button>
       </div>
     );
   });
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,4 +1,5 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const DEL_BOOK = 'bookStore/books/DEL_BOOK';
 
 const initialState = [];
 
@@ -7,11 +8,17 @@ export const addBook = (payload) => ({
   payload,
 });
 
+export const delBook = (payload) => ({
+  type: DEL_BOOK,
+  payload,
+});
+
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
-
+    case DEL_BOOK:
+      return state.filter((item) => item.id !== action.payload);
     default:
       return state;
   }
